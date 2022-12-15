02a028a7bebc00bcd656ddc14d22852cef9bb71c
Oakwood811 <bakwanspencer@ymail.com>
2022-12-15 08:29:00 -0500

    done

---
+++ My projects/Weather App/script.js
@@ -0,0 +1,11 @@
+//Baby weather app (conditionals)
+// if rain then "Go grab your umbrella"
+// else : " Wear your sunglass"
+
+let weather = 'sunny'
+if (weather == 'rain'){
+  console.log('Go grab your umbrella')
+}
+else {
+  console.log('Wear your sunglasses')
+}
